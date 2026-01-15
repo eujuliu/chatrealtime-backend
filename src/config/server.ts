eab3dff23eeee02af1169c62bffdba139415d6f3
Julio Martins <contact.juliomartins@gmail.com>
2026-01-15 16:35:30 -0300

    feat: return static via express

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,8 +3,13 @@ import express from 'express';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
 import { routes } from 'app/api/v1';
-import 'infra/mongodb';
+import path, { dirname } from 'path';
 
+import 'infra/mongodb';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -17,7 +22,12 @@ app.use(
     origin: '*',
   }),
 );
+app.use(express.static(path.join(__dirname, '../../static/')));
 app.use(express.json());
 app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../static/index.html'));
+});
+
 
 export { server, io };
